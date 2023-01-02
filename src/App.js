beb084ca39a9a1b7823df1f4af34bc6dba26ea1f
taqiudind <d.hazky@gmail.com>
2023-01-02 10:23:54 +0700

    sudahSelesai

--- src/App.js
+++ src/App.js
@@ -2,25 +2,37 @@ import "./App.css";
 import { useState } from "react";
 
 const App = () => {
-  const endpoint = process.env.REACT_APP_ENDPOINT;
-
   const [listApi, setListApi] = useState([]);
+
+  const endpoint = process.env.REACT_APP_ENDPOINT;
 
   async function hitApi() {
     const api = await fetch(endpoint);
     const data = await api.json();
-    setListApi(data.data);
+    return data.data;
   }
 
   const showApi = () => {
-    console.log(listApi);
+    hitApi().then((response) => {
+      setListApi(response);
+      console.log(response);
+    });
   };
-
 
   return (
     <div className="App">
       <h1>TES API</h1>
-      <p></p>
+      <div>
+        {listApi.map((data) => {
+          return (
+            <div key={data.id}>
+              <div>{data.email}</div>
+              <div>{data.first_name}</div>
+              <div>{data.last_name}</div>
+            </div>
+          );
+        })}
+      </div>
       <button onClick={() => showApi()}>GET API NOW!!</button>
     </div>
   );
